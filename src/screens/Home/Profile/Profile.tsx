26cf5c650979d36e5b4a99febba4172111406c38
Anton Tobias Jensen <anno196@gmail.com>
2021-01-11 21:33:30 +0100

    ss

--- src/screens/Home/Profile/Profile.tsx
+++ src/screens/Home/Profile/Profile.tsx
@@ -2,12 +2,10 @@ import { RouteProp } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
 import * as React from 'react'
 import { Button, Text } from 'react-native'
-import { useDispatch, useSelector } from 'react-redux'
 
 import ExampleCircle from '../../../../assets/examplecircle.svg'
 import { BlueButton } from '../../../components/atoms/BlueButton'
 import Container from '../../../components/atoms/Container'
-import { Dispatch, RootState } from '../../../state/store'
 import { navigate } from '../../../utils/navigationRef'
 import { HomeStackParams } from '../HomeStackParams'
 
@@ -20,13 +18,10 @@ type Props = {
 
 const Profile: React.FC<Props> = (p) => {
 
-    const dispatch = useDispatch<Dispatch>()
-    const count = useSelector((storeState: RootState) => storeState.test.count)
-
     return (
         <Container background="white">
-            <Button title="hej" onPress={() => { dispatch.test.increment() }} />
-            <Text>{count}</Text>
+            <Button title="hej" onPress={() => console.log("hi")} />
+            <Text>{10}</Text>
             <ExampleCircle />
             <BlueButton text="push to settings" onPress={() => { p.navigation.push('Settings') }} />
             <BlueButton text="push to welcome" onPress={() => { navigate('Welcome') }} />
