1ce786e33410ef158e8312b4551dba39196c81b0
Anton Tobias Jensen <anno196@gmail.com>
2021-01-11 22:37:35 +0100

    initial datastore setup

--- src/screens/Home/Profile/Profile.tsx
+++ src/screens/Home/Profile/Profile.tsx
@@ -1,15 +1,16 @@
+// eslint-disable-next-line simple-import-sort/sort
 import { RouteProp } from '@react-navigation/native'
 import { StackNavigationProp } from '@react-navigation/stack'
+import { DataStore } from '@aws-amplify/datastore'
 import * as React from 'react'
 import { Button, Text } from 'react-native'
-import { DataStore } from "aws-amplify/datastore";
 
 import ExampleCircle from '../../../../assets/examplecircle.svg'
 import { BlueButton } from '../../../components/atoms/BlueButton'
 import Container from '../../../components/atoms/Container'
+import { Click } from '../../../models'
 import { navigate } from '../../../utils/navigationRef'
 import { HomeStackParams } from '../HomeStackParams'
-import { Click } from '../../../models';
 
 type ScreenRouteProp = RouteProp<HomeStackParams, 'Profile'>
 type ScreenNavigationProp = StackNavigationProp<HomeStackParams, 'Profile'>
@@ -20,25 +21,32 @@ type Props = {
 
 const Profile: React.FC<Props> = (p) => {
 
-    const [latest, setLatest] = React.useState<number>(Date.now())
-    const run = () async => {
-        await DataStore.save(
-            new Click({
-              title: "My First Post"
-            })
-          );
-    };
+    const [latestClick, setLatestClick] = React.useState<number>(Date.now())
+    const [allCLicks, setAllCLicks] = React.useState<Click[]>()
+
     React.useEffect(() => {
-        console.log("HEY")
-    }, [latest])
+        (async () => setAllCLicks((await DataStore.query(Click)).reverse()))()
+    }, [])
+
+    React.useEffect(() => {
+        (async () => {
+            const element = await DataStore.save(
+                new Click({
+                    epocTime: Date.now()
+                })
+            )
+            setAllCLicks((prevState) => (prevState ? [element, ...prevState] : [element]))
+        })()
+    }, [latestClick])
 
     return (
         <Container background="white">
-            <Button title="Try me" onPress={() => setLatest(Date.now())} />
-            <Text>{latest}</Text>
+            <Button title="Try me" onPress={() => setLatestClick(Date.now())} />
+            <Text>{latestClick}</Text>
             <ExampleCircle />
             <BlueButton text="push to settings" onPress={() => { p.navigation.push('Settings') }} />
             <BlueButton text="push to welcome" onPress={() => { navigate('Welcome') }} />
+            {allCLicks && allCLicks.map(({ epocTime }, key) => (<Text key={key}>{epocTime}</Text>))}
         </Container>
     )
 }
